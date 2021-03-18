415d54e645b9c2a14901e709ecad1a00ce6fda09
esteban <tebas_tebas@hotmail.com>
2021-03-18 12:57:28 +0100

    comenzamos Java script

---
+++ Temas _5_Javascript/Cheatsheet/Js/main.js
@@ -0,0 +1,19 @@
+/*------------------Uso de Js externo------------ */
+function saludo (){
+    alert ("hola desde un elemento externo")
+    console.log("has hecho click");
+}
+document.getElementsByTagName("a").item(2).addEventListener("click", saludo);
+
+
+
+
+/*------------------depuracion 7 logging---------------*/
+// alert("mensaje");
+console.log("mensaje");
+
+
+/*------------------Variables---------------*/
+
+let 
+
