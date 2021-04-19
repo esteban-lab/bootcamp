4ed99b1514d44c3e9495f74c5311699a7e93db9e
esteban <tebas_tebas@hotmail.com>
2021-04-19 13:55:53 +0200

    React parte 1

---
+++ TEMA_6_REACT/cheatsheet/my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import FirstComponent from './components/FirstComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ TEMA_6_REACT/cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,11 @@
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Titulo</h3>
+            <small>16 de Agosto de 2025</small>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+}
+
+// export default FirstComponent
