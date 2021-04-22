9c9ef040c0f6b5a70f44f79bc3fd8c6ad50df792
esteban <tebas_tebas@hotmail.com>
2021-04-22 14:04:45 +0200

    ejercicio3 resuelto

--- TEMA_6_REACT/ejercicios/ejercicio3/src/App.js
+++ TEMA_6_REACT/ejercicios/ejercicio3/src/App.js
@@ -1,26 +1,27 @@
-
+import {useState} from "react";
 import './App.css';
 import Agenda from "./components/Agenda";
 import Formulario from "./components/Formulario";
 
-function App(props) {
-  const contactos = {
+function App() {
 
-    datos: [
-        {id:1, nombre: "Juan", Apellidos: "Ayala Vega", direccion: "calle panorama 666", ciudad:"Monaco", cp: 39835, tlf: 677708990}, 
-        {id:2, nombre: "estebitan", Apellidos: "lurin janampa", direccion: "calle panorama 456", ciudad:"Osaka", cp: 303235, tlf: 645608990},
-        {id:3, nombre: "Juan", Apellidos: "Miro Quesada", direccion: "calle infiernillo 69", ciudad:"Krakobia", cp: 32355, tlf: 678708990}
-    ]
-  };
 
+  const InitialContactosState = [
+
+        {id:1, nombre: "Pompeyo", apellidos: "Ayala Vega", direccion: "calle panorama 666", ciudad:"Monaco", cp: 398035, tlf: 677708990}, 
+        {id:2, nombre: "Seya", apellidos: "lurin janampa", direccion: "calle panorama 456", ciudad:"Osaka", cp: 303235, tlf: 645608990},
+        {id:3, nombre: "Esmeregildo", apellidos: "Miro Quesada", direccion: "calle infiernillo 69", ciudad:"Krakobia", cp: 32355, tlf: 678708990}
+    ];
+
+  const [contactos, setContactos] = useState(InitialContactosState);  
 
   return (
-    <div className="App">
-      <h3 className="h3">Contact List</h3>
-      <Agenda data={contactos.datos}/>
-      <h3 className="h3">New Contact</h3>
-      <Formulario/>
-    
+    <div className="container">
+      <h3 className="my-4">Contact List</h3>
+      <Agenda contactos={contactos}/>
+
+      <h3 className="my-4">New Contact</h3>
+      <Formulario setContactos={setContactos}/>
     </div>
   );
 }
--- TEMA_6_REACT/ejercicios/ejercicio3/src/components/Agenda.js
+++ TEMA_6_REACT/ejercicios/ejercicio3/src/components/Agenda.js
@@ -1,25 +1,18 @@
-
-import React from 'react'
-
-export default function Agenda(props) {
+export default function Agenda({ contactos }) {
+    console.log(contactos);
     return (
-    <div className ="row align-items-start d-flex justify-content-around">
-      
-            {
-                props.data.map(contacto =>{
-
-                    return <ul key={contacto.id}>
-                        <li className="list-group-item active h6" aria-current="true">contacto {contacto.id}</li>
-                        <li className="list-group-item">{contacto.nombre}</li>
-                        <li className="list-group-item">{contacto.apellidos}</li>
-                        <li className="list-group-item">{contacto.telefono}</li>
-                        <li className="list-group-item">{contacto.direccion},{contacto.cp},{contacto.ciudad}</li>
-                        <li className="list-group-item"><button type="button" class="btn btn-warning">Eliminar</button></li>
-                    </ul>
-                }
-                    )
-            }
-        
-    </div>
+        <div className="row">
+            {contactos.map((contacto, index) => {
+                return (
+                <ul className="list-group mb-3 col-12 col-sm-6 col-xl-3" key={contacto.tlf}>
+                    <li className="list-group-item active">Contacto {index + 1}</li>
+                    <li className="list-group-item">{contacto.nombre}</li>
+                    <li className="list-group-item">{contacto.apellidos}</li>
+                    <li className="list-group-item">{contacto.tlf}</li>
+                    <li className="list-group-item">{contacto.direccion}, {contacto.cp}, {contacto.ciudad}</li>
+                </ul>
+                );
+            })}
+        </div>
     )
-}
+    }
