0df0b6ccbd5d887f24acf9ceb118637fa29efbdc
esteban <tebas_tebas@hotmail.com>
2021-04-23 14:00:45 +0200

    ejercicio4

---
+++ TEMA_6_REACT/ejercicios/ejercicio4/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import List from "./components/List";
+import Input from "./components/Input";
+
+function App() {
+
+  const todoo = []
+  
+  
+  fetch('https://raw.githubusercontent.com/BC-FSWD/todo-list/master/todo-list.json')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  return (
+    <div className="App">
+     <h3>TO-DO LIST</h3>
+     <List/>
+     <Input type="text" placeholder="Introduce tu nuevo to do"/>
+    </div>
+  );
+}
+
+export default App;
